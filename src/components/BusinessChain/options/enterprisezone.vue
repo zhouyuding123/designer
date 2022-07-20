ed05a1fb76cd26ad02e3dd146cb88f8c847acd1e
zhouyuding <3487379059@qq.com>
2022-07-20 21:11:05 +0800

    s 0.0.4

--- src/components/BusinessChain/options/enterprisezone.vue
+++ src/components/BusinessChain/options/enterprisezone.vue
@@ -22,11 +22,39 @@
     </div>
     <div class="company">
       <div class="companyback">
-        <div v-for="item in valueListE" :key="item.id">
-          <div class="company_line">123</div>
-          <div class="brder_line">
+        <div v-for="(item,index) in valueListE" :key="index">
+        <div class="brder_line" v-if="index != 0">
             <div class="brder_line_line"></div>
           </div>
+          <div class="company_line">
+            <div class="company_line_img">
+              <img
+                v-if="item.headimage"
+                :src="imagesValue + item.headimage"
+                alt=""
+              />
+              <img
+                v-if="item.headimage == null || item.headimage == ''"
+                :src="imagesValue + notimg"
+                alt=""
+              />
+            </div>
+            <div>
+              <div class="company_linenick">
+                <div class="company_linenick_name">{{ item.nickname }}</div>
+                <div class="company_linenick_img">
+                  <img src="@/assets/imgers/认证.png" alt="" />
+                </div>
+              </div>
+              <div v-if="item.province" class="dzhi">
+                {{ item.province }}{{ item.city }}{{ item.area
+                }}{{ item.detail }}
+              </div>
+              <div v-else class="dzhi">外太空</div>
+            </div>
+            <div></div>
+          </div>
+          
         </div>
       </div>
     </div>
@@ -36,9 +64,12 @@
 <script>
 import { getListApi, companyGetListApi } from "@/urls/wsUrl.js";
 import { postD } from "@/api";
+import { imgUrl } from "@/assets/js/modifyStyle";
 export default {
   data() {
     return {
+      notimg: "images/20220720/165832004615c379d82e26251591b1e85c8e4fdf7e.jpg",
+      imagesValue: "",
       getList: [],
       Listvalue: {
         city: "",
@@ -48,23 +79,24 @@ export default {
         company_main: "",
       },
       valueListE: [],
-      company_mainId: 0
+      company_mainId: 0,
     };
   },
   created() {
     this.getListApi();
     this.enterpriseList();
+    this.imagesValue = imgUrl();
   },
   methods: {
     getListApi() {
-        this.Listvalue.company_main=""
+      this.Listvalue.company_main = "";
       postD(getListApi()).then((res) => {
         this.getList = res.list;
       });
     },
     freelists(val) {
       console.log(val);
-      this.company_mainId =""
+      this.company_mainId = "";
     },
     enterpriseList() {
       postD(companyGetListApi()).then((res) => {
